216806c06a786d0d8d251d8add00f6c180916d8c
Dmitry <dimonbyt@gmail.com>
2023-05-17 23:51:05 +0300

    Add project files.

---
+++ ChatWithSignalR/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMesaage", function (fromUser, message) {
+    var msg = fromUser + ": " + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+});
+
+connection.start();
+
+$("#btnSend").on("click", function () {
+    var fromUser = $("#textUser").val();
+    var message = $("#txtMsg").val();
+
+    connection.invoke("SendMessage", fromUser, message);
+});
